97cc5ad25fab2c470d3408891ec9863b6e62ef02
Matias Alejo Garcia <ematiu@gmail.com>
2015-09-15 10:00:29 -0300

    v0.4.0

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var bwcModule = angular.module('bwcModule', []);
 var Client = require('bitcore-wallet-client');
 
-bwcModule.constant('MODULE_VERSION', '0.3.0');
+bwcModule.constant('MODULE_VERSION', '0.4.0');
 
 bwcModule.provider("bwcService", function() {
   var provider = {};
