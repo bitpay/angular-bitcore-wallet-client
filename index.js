c2ec33b79460725809217bd26d6e3d2149b2fa08
Matias Alejo Garcia <ematiu@gmail.com>
2015-11-10 18:29:31 -0300

    ver 116

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var bwcModule = angular.module('bwcModule', []);
 var Client = require('bitcore-wallet-client');
 
-bwcModule.constant('MODULE_VERSION', '1.1.5');
+bwcModule.constant('MODULE_VERSION', '1.1.6');
 
 bwcModule.provider("bwcService", function() {
   var provider = {};
@@ -38,6 +38,8 @@ bwcModule.provider("bwcService", function() {
     service.getSJCL = function() {
       return Client.sjcl;
     };
+
+    service.buildTx = Client.buildTx;
 
     service.getUtils = function() {
       return Client.Utils;
